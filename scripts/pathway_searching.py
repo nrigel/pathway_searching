eacc7ba017becab80913699cdd75a76237c774c7
nrigel <nick.rigel@gmail.com>
2020-04-14 22:44:44 -0400

    Update pathway_searching.py

--- scripts/pathway_searching.py
+++ scripts/pathway_searching.py
@@ -33,41 +33,35 @@ class PathwayMapper(object):
     def submotifmatcher(self, C, H):
         with NMRT._driver.session() as db:
             # submotif.CS = [replicates, Cavg, Cstd, Havg, Hstd]
-            #print(db.run('MATCH (s:submotif_1) RETURN s.CS').value())
-            
+            matches = {}
             for i in range(self.r[0], self.r[1]):
                 Cavg = list(self.csaverages[i].keys())[0]
                 Havg = self.csaverages[i][Cavg]
+                Cavg, Havg = 1.9005, 0.1165
                 a = '\n'.join(['WITH '+str(C)+' AS Cexp, '+str(H)+' AS Hexp, '+str(Cavg)+' AS Cavg, '+str(Havg)+' AS Havg, '+str(self.matching_cutoff)+' as cutoff',
-                'MATCH (s:submotif_'+str(i)+') RETURN', 'CASE', 'WHEN toInteger(split(s.CS, " ")[0]) >= '+str(self.replicates),
-                'THEN CASE', 'WHEN toInteger(split(s.CS, " ")[2]) > 0 AND toInteger(split(s.CS, " ")[4]) > 0',
-                'THEN CASE', 'WHEN (abs(Cexp-toFloat(split(s.CS, " ")[2]))/toFloat(split(s.CS, " ")[2])+abs(Hexp-toFloat(split(s.CS, " ")[4]))/toFloat(split(s.CS, " ")[4]))/2 <= cutoff',
-                'THEN [s.SMILES, (abs(Cexp-toFloat(split(s.CS, " ")[2]))/toFloat(split(s.CS, " ")[2])+abs(Hexp-toFloat(split(s.CS, " ")[4]))/toFloat(split(s.CS, " ")[4]))/2] END',
-                'WHEN toInteger(split(s.CS, " ")[2]) = 0 AND toInteger(split(s.CS, " ")[4]) > 0',
-                'THEN CASE', 'WHEN (abs(Cexp-Cavg)/Cavg+abs(Hexp-toFloat(split(s.CS, " ")[4]))/toFloat(split(s.CS, " ")[4]))/2 <= cutoff',
-                'THEN [s.SMILES, (abs(Cexp-Cavg)/Cavg+abs(Hexp-toFloat(split(s.CS, " ")[4]))/toFloat(split(s.CS, " ")[4]))/2] END',
-                'WHEN toInteger(split(s.CS, " ")[2]) > 0 AND toInteger(split(s.CS, " ")[4]) = 0', 'THEN CASE',
-                'WHEN (abs(Cexp-toFloat(split(s.CS, " ")[2]))/toFloat(split(s.CS, " ")[2])+abs(Hexp-Havg)/Havg)/2 <= cutoff',
-                'THEN [s.SMILES, (abs(Cexp-toFloat(split(s.CS, " ")[2]))/toFloat(split(s.CS, " ")[2])+abs(Hexp-Havg)/Havg)/2] END',
-                'END', 'ELSE CASE', 'WHEN (abs(Cexp-Cavg)/Cavg+abs(Hexp-Havg)/Havg)/2 <= cutoff',
-                'THEN [s.SMILES, (abs(Cexp-Cavg)/Cavg+abs(Hexp-Havg)/Havg)/2] END END AS result'])
-
-                matches = {m[0][0]: m[0][1] for m in db.run(a).values() if m[0] != None}
-                print(matches, '\n')
-                
-
-                        
-                
-
-
-
+                                'MATCH (s:submotif_'+str(i)+') RETURN', 'CASE', 'WHEN toInteger(split(s.CS, " ")[0]) >= '+str(self.replicates),
+                                'THEN CASE', 'WHEN toInteger(split(s.CS, " ")[2]) > 0 AND toInteger(split(s.CS, " ")[4]) > 0',
+                                'THEN CASE', 'WHEN (abs(Cexp-toFloat(split(s.CS, " ")[1]))/toFloat(split(s.CS, " ")[2])+abs(Hexp-toFloat(split(s.CS, " ")[3]))/toFloat(split(s.CS, " ")[4]))/2 <= cutoff',
+                                'THEN [s.SMILES, (abs(Cexp-toFloat(split(s.CS, " ")[1]))/toFloat(split(s.CS, " ")[2])+abs(Hexp-toFloat(split(s.CS, " ")[3]))/toFloat(split(s.CS, " ")[4]))/2] END',
+                                'WHEN toInteger(split(s.CS, " ")[2]) = 0 AND toInteger(split(s.CS, " ")[4]) > 0',
+                                'THEN CASE', 'WHEN (abs(Cexp-toFloat(split(s.CS, " ")[1]))/Cavg+abs(Hexp-toFloat(split(s.CS, " ")[3]))/toFloat(split(s.CS, " ")[4]))/2 <= cutoff',
+                                'THEN [s.SMILES, (abs(Cexp-toFloat(split(s.CS, " ")[1]))/Cavg+abs(Hexp-toFloat(split(s.CS, " ")[3]))/toFloat(split(s.CS, " ")[4]))/2] END',
+                                'WHEN toInteger(split(s.CS, " ")[2]) > 0 AND toInteger(split(s.CS, " ")[4]) = 0', 'THEN CASE',
+                                'WHEN (abs(Cexp-toFloat(split(s.CS, " ")[1]))/toFloat(split(s.CS, " ")[2])+abs(Hexp-toFloat(split(s.CS, " ")[3]))/Havg)/2 <= cutoff',
+                                'THEN [s.SMILES, (abs(Cexp-toFloat(split(s.CS, " ")[2]))/toFloat(split(s.CS, " ")[2])+abs(Hexp-Havg)/Havg)/2] END',
+                                'END', 'ELSE CASE', 'WHEN (abs(Cexp-toFloat(split(s.CS, " ")[1]))/Cavg+abs(Hexp-toFloat(split(s.CS, " ")[3]))/Havg)/2 <= cutoff',
+                                'THEN [s.SMILES, (abs(Cexp-toFloat(split(s.CS, " ")[1]))/Cavg+abs(Hexp-Havg)/toFloat(split(s.CS, " ")[3]))/2] END END AS result'])
+                matches[i] = {m[0][0]: m[0][1] for m in db.run(a).values() if m[0] != None}
+            return matches
 
     def spinsystem(self, ss):
         # first search for sub-motif matches
+        print(self.csaverages)
         for node in ss:
             for C in ss[node]:
-                self.submotifmatcher(C, ss[node][C])
-            break
+                matches = self.submotifmatcher(C, ss[node][C])
+                
+                print(node, len(matches[2]))
 
     
 # example spin-systems
