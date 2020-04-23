9672668628c84c226ddf35cc08dad715b3feb7e7
nrigel <nick.rigel@gmail.com>
2020-04-23 16:49:48 -0400

    423

--- webserver/Scripts/pathway_search.js
+++ webserver/Scripts/pathway_search.js
@@ -1,13 +1,12 @@
 var relative_dir; //relative_dir on the server, needed for xhttp call
 var session_id;
 
-var for_debug;
 
-var key_flag = 0;
-
-//var neo4j = require('neo4j-driver');
-var target;
-
+var pathway_data = {}; // pathway checkbox ids to nodes and edges
+var pathway_checkboxes = {}; // doc ids for userfile to pathway checkbox ids
+var cy_layout = "random"; // layout option for cytoscape graphing
+var node_labels = "name"; // node attribute that is displayed on cytoscape graph
+var edge_labels = "type"; // edge labels from Reactome
 
 document.addEventListener('keydown', function(event) {
 
@@ -23,44 +22,11 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-
-function query_database(flag){
-    var oReq = new XMLHttpRequest();
-    var base_url = window.location.href;
-
-    var params = new FormData(document.getElementById("process_load"));
-    
-    var userfile = document.getElementById("fileinput").value
-
-    //document.getElementById("infor").innerHTML = "Load your spectrum from the server. Please be patient.";
-    if (document.getElementById("fileinput").value) {
-
-        oReq.open("POST", base_url.concat('Scripts/pathway_search.py'), true);
-        oReq.setRequestHeader("Authorization", null);
-        
-        oReq.onload = function(oEvent) {
-            //result = JSON.parse(oReq.responseText.replace(/\bNaN\b/g, "null"));
-            window.alert(oReq.responseText);
-            if (oReq.status === 200) {
-                //alert(oReq.responseText);
-                //try{
-                result = JSON.parse(oReq.responseText.replace(/\bNaN\b/g, "null"));
-            }
-        }
-        oReq.send(params);
-        window.alert(userfile)
-        
-   } else {
-    window.alert('No file selected...')
-   }
-    
-}
-
-
-
 function check_change(flag) {
     if (flag === "motifs") {
+       
         if (document.getElementById("motifs").checked) {
+            console.log(document.getElementById("motifs").onchange);
             document.getElementById("motif_0").checked = true;
             document.getElementById("motif_1").checked = true;
             document.getElementById("motif_2").checked = true;
@@ -84,8 +50,169 @@ function check_change(flag) {
             document.getElementById("submotif_4").checked = false;
         }
     }
+    // pathway list check boxes
+    if (flag.slice(0, 2) === "pl") {
+        var checked = document.getElementById(flag).checked;
+        if (flag in pathway_checkboxes) {
+            var plist = pathway_checkboxes[flag];
+            for (i = 0; i < plist.length; i++) { 
+                if (checked) {
+                    if (document.getElementById(plist[i]).checked === false) {
+                        document.getElementById(plist[i]).checked = true;
+                        check_change(plist[i]);
+                    }
+                } else {
+                    if (document.getElementById(plist[i]).checked) {
+                        document.getElementById(plist[i]).checked = false;
+                        check_change(plist[i]);
+                    }
+                }
+            }
+        } else {
+            if (checked) {
+                visualize(flag, pathway_data[flag].nodes, pathway_data[flag].edges);
+            } else {
+                de_visualize(flag, pathway_data[flag].nodes);
+            }            
+        }
+    }
 }
+
+function query_database(flag){
+    var oReq = new XMLHttpRequest();
+    var base_url = window.location.href;
+
+    var params = new FormData(document.getElementById("process_load"));
     
-function applyfilters() {
-    target = [];
+    var userfile = document.getElementById("fileinput").value
+
+    //document.getElementById("infor").innerHTML = "Load your spectrum from the server. Please be patient.";
+    if (userfile) {
+
+        oReq.open("POST", base_url.concat('Scripts/pathway_search.py'), true);
+        oReq.setRequestHeader("Authorization", null);
+        
+        oReq.onload = function(oEvent) {
+            //window.alert(oReq.responseText);
+            if (oReq.status === 200) {
+                //console.log(oReq.response);
+                var result = JSON.parse(oReq.response);
+                var pathway_ids = set_pathwaylist(userfile, result.pathway_list)
+                for (i = 0; i < result.pathway_list.length; i++) { 
+                    pathway_data[pathway_ids[i]] = result.pathway_data[result.pathway_list[i]];
+                    var nodes = pathway_data[pathway_ids[i]].nodes;
+                    var edges = pathway_data[pathway_ids[i]].edges;
+                    for (j = 0; j < nodes.length; j++) {
+                        nodes[j].data["js_pathway_id"] = pathway_ids[i];
+                        // add specific pathway id into node ids
+                        var node_id = nodes[j].data["id"].toString();
+                        nodes[j].data["id"] = node_id.concat('_').concat(pathway_ids[i]);
+                    }
+                    for (j = 0; j < edges.length; j++) {
+                        edges[j].data["js_pathway_id"] = pathway_ids[i];
+                        // add specific pathway id into node ids
+                        var source_id = edges[j].data['source'].toString();
+                        var target_id = edges[j].data['target'].toString();
+                        edges[j].data['source'] = source_id.concat('_').concat(pathway_ids[i]);
+                        edges[j].data['target'] = target_id.concat('_').concat(pathway_ids[i]);
+                    }
+                }
+        }   }
+        oReq.send(params);
+        window.alert("Searching for Pathways...");
+        document.getElementById("fileinput").value = userfile
+    } else {
+        window.alert("No file selected...")
+   }
+    
+}
+
+function set_pathwaylist(userfile, pathway_list) {
+    var ul = document.getElementById("pathway_list");
+    var filename = userfile.split(/(\\|\/)/g).pop();
+    if (document.getElementById("pl_".concat(filename)) == null) {
+        var l = document.createElement("ol");
+        l.setAttribute("id", "pl_".concat(filename));
+        l.setAttribute("style", "list-style-type:none; padding-left: 15px;");
+
+        var id = "plbox_".concat(filename)
+        pathway_checkboxes[id] = [];
+        var x = document.createElement("input");
+        x.setAttribute("type", "checkbox");
+        x.setAttribute("id", id);
+        x.setAttribute("onclick", "check_change('".concat(id).concat("')"));
+        var label = document.createElement("label");
+        label.htmlFor = id;
+        label.appendChild(document.createTextNode(filename));
+        ul.appendChild(x);
+        ul.appendChild(label);
+        ul.appendChild(l);
+        ul.appendChild(document.createElement("br"));
+    }
+    
+    var pathway_ids = {};
+    var l = document.getElementById("pl_".concat(filename));
+    var i;
+    for (i = 0; i < pathway_list.length; i++) { 
+        var id = null;
+        var c = 0;
+        while (id == null) {
+            var s = "plbox_".concat(filename).concat("_").concat(pathway_list[i])
+            if (c) {
+                s = s.concat("_").concat(c);
+            }
+            if (document.getElementById(s) == null) {
+                var id = s;
+            } else {
+                c += 1;
+            }
+        }
+        var x = document.createElement("input");
+        x.setAttribute("type", "checkbox");
+        x.setAttribute("id", id);
+        x.setAttribute("onclick", "check_change('".concat(id).concat("')"));
+        var label = document.createElement("label");
+        label.htmlFor = id;
+        label.appendChild(document.createTextNode(pathway_list[i]));
+        l.appendChild(x);
+        l.appendChild(label);
+        l.appendChild(document.createElement("br"));
+        pathway_ids[i] = id;
+        pathway_checkboxes["plbox_".concat(filename)].push(id);
+    }
+    return pathway_ids
+}
+
+function clear_pathwaylist() {
+    const ul = document.getElementById("pathway_list");
+    ul.innerHTML = "";
+}
+
+function setup_cy(container_id) {
+    var cy = window.cy = cytoscape({
+                container: document.getElementById('cy'),
+
+                style: [
+                    {selector: 'node',
+                        style: {'content': 'data('.concat(node_labels).concat(')')}},
+
+                    {selector: 'edge',
+                        style: {'label': 'data('.concat(edge_labels).concat(')'),
+                            'curve-style': 'bezier',
+                            'target-arrow-shape': 'triangle'
+                        }}],
+                elements: {nodes: [], edges: []}, layout: {name: cy_layout}});
+}
+
+function visualize(pathway, nodes, edges) {
+    // this will overlap the nodes if the same node label already exists...
+    cy.add(nodes);
+    cy.add(edges);
+    var layout = cy.layout({name: cy_layout});
+    layout.run();
+}
+
+function de_visualize(pathway, nodes) {
+    cy.remove(cy.elements('node[js_pathway_id = "'.concat(pathway).concat('"]')));
+    cy.remove(cy.elements('edge[js_pathway_id = "'.concat(pathway).concat('"]')));
 }
--- webserver/Scripts/pathway_search.py
+++ webserver/Scripts/pathway_search.py
@@ -1,33 +1,143 @@
 #!/opt/anaconda2/envs/P3/bin/python
-
-import cgi, cgitb 
+import cgi, cgitb, sys, math, csv, json
+import networkx as nx
+sys.path.append('../../scripts')
+from neo4j_connect import NMRTServer, ReactomeServer
 
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 
 print("Content-type:text/html\r\n\r\n")
-print('<html>')
-print('<head>')
-print('</head>')
-print('<body>')
 
 # Get data from fields
-#print(form.keys())
-datafile = form.getvalue('userfile')
-structure_opts = {o: form.getvalue(o) for o in ['metabolites', 'motif_0', 'motif_1', 'motif_2', 'submotif_1', 'submotif_2', 'submotif_3', 'submotif_4']}
+datafile = form['userfile'].file
+structure_opts = [o for o in ['metabolites', 'motif_0', 'motif_1', 'motif_2', 'submotif_1', 'submotif_2', 'submotif_3', 'submotif_4'] if form.getvalue(o)]
 species = form.getvalue('specieslist')
 count_cutoff = form.getvalue('cutoff_count')
-#print(datafile)
-print(structure_opts)
-print(species)
-print(count_cutoff)
+
+# Read in data file
+lines = []
+while True:
+    line = datafile.readline()
+    if not line:
+        break
+    lines.extend([line])
+data_set = [line.rstrip().decode('utf-8').split(',') for line in lines]
+
+# Format metabolites dict
+names_to_colmarid = {}
+with open('/Users/nickrigel/Documents/GitHub/pathway_searching/updating_Reactome_molecules/name_to_colmarid.csv') as csvin:
+    csvin.readline()
+    for row in csv.reader(csvin):
+        names_to_colmarid[row[0]] = row[1]
+
+metabolites = {}
+for i in range(1, len(data_set)):
+    metabolite, p_value, fold_change = data_set[i][0], -math.log(float(data_set[i][1])), float(data_set[i][2])
+    if metabolite in names_to_colmarid:
+        colmar = names_to_colmarid[metabolite]
+    else:
+        colmar = metabolite    
+    metabolites[colmar] = {'COLMAR': colmar, 'Fold Change': fold_change, 'P-Value': p_value, 'Metabolite': metabolite}
+
+# 1. Search for pathways with any of the inputted metabolites
+Reactome = ReactomeServer()
+pathways = [{}, [], [], []]
+with Reactome._driver.session() as db:
+    for metabolite in metabolites:
+        pathways[0][metabolite] = set()
+        colmar = metabolites[metabolite]['COLMAR']
+        fold_change = metabolites[metabolite]['Fold Change']
+        for (m, p) in db.run('MATCH (m:ReferenceMolecule)-[me:referenceEntity]-(e:PhysicalEntity)-[er:input]-(r:Reaction)-[rp:hasEvent]-(p:Pathway) WHERE "'+colmar+'" IN m.COLMAR AND p.speciesName = "Homo sapiens" RETURN ([m.displayName, p.displayName])').value():
+            pathways[0][metabolite].add(p)
+# 2. Generate list of pathways
+for metabolite in pathways[0]:
+    for pathway in pathways[0][metabolite]:
+        pathways[1].extend([pathway])
+
+# 3. Sorte pathway list by most occuring coming first
+pathways[1] = sorted(pathways[1], key=lambda x: -pathways[1].count(x))
+
+# 4. Generate list of pathways in the order from step 3 with no repeats
+for p in pathways[1]:
+    if p not in pathways[2]:
+        pathways[2].extend([p])
+
+# 5. Iterate thru pathway list and compile new list containing pathways that have "n" number of metabolites
+for p in pathways[2]:
+    count = pathways[1].count(p)
+    if count > 3:
+        pathways[3].extend([p])
+
+# 6. Search for all reactions in each of those pathways
+pathway_graphs = {}
+with Reactome._driver.session() as db:
+    # https://neo4j.com/docs/api/python-driver/current/types/graph.html
+    entities_to_metabolites = {}
+    #for pathway in pathways[3]:
+    #pathways[3] = ["Citric acid cycle (TCA cycle)"]
+    pathway_names, pathway_data = [], {}
+    for pathway in pathways[3]:
+        pathway_graphs[pathway] = nx.DiGraph()
+
+        command = 'MATCH (p:Pathway) WHERE p.displayName = "'+pathway+'" AND p.speciesName = "Homo sapiens"'
+        command = command+'\n'+'MATCH (R1:Reaction)-[pe:precedingEvent]-(R2:Reaction) WHERE (R1)-[:hasEvent]-(p)  AND (R2)-[:hasEvent]-(p) '
+        command = command+'\n'+'RETURN R1, R2, pe'
+
+        reactions = db.run(command).graph()
+
+        if reactions.nodes:
+            #print(pathway, '\n')
+            for node in reactions.nodes:
+                if 'Reaction' in list(node.labels): 
+                    node_dict = dict(node)
+                    node_dict['labels'], node_dict['TYPE'] = list(node.labels), 'Reaction'
+                    pathway_graphs[pathway].add_nodes_from([(node['dbId'], node_dict)])
+
+                    command = 'MATCH (p:Pathway) WHERE p.displayName = "'+pathway+'" AND p.speciesName = "Homo sapiens"'
+                    command = command+'\n'+'MATCH (m:ReferenceMolecule)-[me:referenceEntity]-(e:PhysicalEntity)-[er]-(r:Reaction)-[rp:hasEvent]-(p) WHERE r.dbId = '+str(node['dbId'])+' RETURN *'
+                    reaction = db.run(command).graph()
+                    
+                    metabolites = [n['displayName'] for n in reaction.nodes if 'ReferenceMolecule' in list(n.labels)]
+                    
+                    # Reaction --> PhysicalEntity --> ReferenceMolecule
+                    # make dictionary of PhysicalEntities to Metabolites
+                    for relationship in reaction.relationships:
+                        if relationship.type == 'referenceEntity':
+                            n1, n2 = relationship.start_node, relationship.end_node # n1 = entity, n2 = metabolite
+                            entities_to_metabolites[n1['dbId']] = n2 
+
+                    # add metabolites to pathway graph by connecting to reactions
+                    for relationship in reaction.relationships:
+                        n1, n2 = relationship.start_node, relationship.end_node # n1 = Reaction, n2 = PhysicalEntity
+                        if relationship.type in ['input', 'output'] and 'Reaction' in list(n1.labels):
+                            n3 = entities_to_metabolites[n2['dbId']] # metabolite node
+                            if n3['dbId'] not in pathway_graphs[pathway]: # add metabolite node to new graph
+                                node_dict = dict(n3)
+                                node_dict['labels'], node_dict['TYPE'] = list(n3.labels), 'Metabolite'
+                                pathway_graphs[pathway].add_nodes_from([(n3['dbId'], node_dict)])
+                            if relationship.type == 'input': # input metabolites go towards reaction
+                                pathway_graphs[pathway].add_edge(n3['dbId'], n1['dbId'], relationship=relationship, TYPE='input')
+                            if relationship.type == 'output': # output metabolites go away from reaction
+                                pathway_graphs[pathway].add_edge(n1['dbId'], n3['dbId'], relationship=relationship, TYPE='output')
+
+            for relationship in reactions.relationships:
+                n2, n1 = relationship.start_node, relationship.end_node
+                # (n1)-[:PrecedingEvent]->(n2); start_node = reaction 2, end_node = reaction 1
+                pathway_graphs[pathway].add_edge(n1['dbId'], n2['dbId'], TYPE='precedingEvent', relationship=relationship)
+
+        G = nx.DiGraph()
+        for node in pathway_graphs[pathway].nodes:
+            G.add_node(node, id=node, name=pathway_graphs[pathway].nodes[node]['displayName'], type=pathway_graphs[pathway].nodes[node]['TYPE'])
+        for edge in pathway_graphs[pathway].edges:
+            G.add_edge(edge[0], edge[1], type=pathway_graphs[pathway].edges[edge]['TYPE'])
+        
+        nodes, edges = [{'data': G.nodes[node], 'group': 'nodes'} for node in G.nodes], [{'data': {'source': edge[0], 'target': edge[1], 'type': G.edges[edge]['type']}, 'group': 'edges'} for edge in G.edges]
+        pathway_name = pathway+' ('+str(pathways[1].count(pathway))+')'
+        pathway_names.extend([pathway_name])
+        pathway_data[pathway_name] = {'nodes': nodes, 'edges': edges}
+
+print(json.dumps({'pathway_data': pathway_data, 'pathway_list': pathway_names}))
 
 
-# Need to figure out how to keep selected file on page reload
-# Need to call search script from here with params
-# Need to return search results
 # Need to figure out how I want to display results
-
-
-print('</body>')
-print('</html>')
