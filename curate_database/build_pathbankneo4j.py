8465e8bbc9e86d02f8c270085e406d6364fbbd6f
nrigel <nick.rigel@gmail.com>
2020-05-16 13:57:24 -0400

    051620

--- curate_database/build_pathbankneo4j.py
+++ curate_database/build_pathbankneo4j.py
@@ -3,70 +3,59 @@
 # edge csvs: ':START_ID',descriptors,':END_ID',':TYPE'
 neo4j_admin_path = '/Applications/neo4j-community-3.5.14_pathbank/bin/'
 path = 'pathbank_data/'
-pathbank_nodes = {'Pathway': {'CSV': 'pathbank_pathways.csv', 'id_index': 0, 'owl_index': 0, 'header': ['dbId:ID', 'SMPDB_ID', 'PW_ID', 'Pathway_Name', 'Pathway_Subject', 'Description', ':LABEL']},
-                'Metabolite': {'CSV': 'pathbank_all_metabolites.csv', 'id_index': 4, 'owl_index': 0, 'header': ['dbId:ID', 'SMPDB_ID', 'Pathway_Name', 'Pathway_Subject', 'Species', 'PW_ID', 'Metabolite_Name', 'HMDB_ID', 'KEGG_ID', 'ChEBI_ID', 'DrugBank_ID', 'CAS', 'Formula', 'IUPAC', 'SMILES', 'InChI', 'InChIKey', ':LABEL']},
-                'Protein': {'CSV': 'pathbank_all_proteins.csv', 'id_index': 4, 'owl_index': 0, 'header': ['dbId:ID', 'SMPDB_ID', 'Pathway_Name', 'Pathway_Subject', 'Species', 'Uniprot_ID', 'Protein_Name', 'HMDBP_ID', 'DrugBank_ID', 'GenBank_ID', 'Gene_Name', 'Locus', ':LABEL']}}
+pathbank_nodes = {'Pathway': {'CSV': 'pathbank_pathways.csv', 'id_index': 0, 'species_index': 4, 'header': ['SMPDB_ID:ID', 'PW_ID', 'Pathway_Name', 'Pathway_Subject', 'Species', 'Description', ':LABEL']},
+                'Metabolite': {'CSV': 'pathbank_all_metabolites.csv', 'id_index': 4, 'species_index': 3, 'header': ['SMPDB_ID', 'Pathway_Name', 'Pathway_Subject', 'Species', 'PW_ID:ID', 'Metabolite_Name', 'HMDB_ID', 'KEGG_ID', 'ChEBI_ID', 'DrugBank_ID', 'CAS', 'Formula', 'IUPAC', 'SMILES', 'InChI', 'InChIKey', ':LABEL']},
+                'Protein': {'CSV': 'pathbank_all_proteins.csv', 'id_index': 4, 'species_index': 3, 'header': ['SMPDB_ID', 'Pathway_Name', 'Pathway_Subject', 'Species', 'Uniprot_ID:ID', 'Protein_Name', 'HMDBP_ID', 'DrugBank_ID', 'GenBank_ID', 'Gene_Name', 'Locus', ':LABEL']}}
 
-IDs, OWLs, node_csvs, edge_csvs = {}, {}, [], []
+node_data, node_csvs, edge_csvs = {}, [], []
+pathway_species = {}
+import csv, json
 
-import csv, pickle, json
-
-for node in pathbank_nodes:
+for node in ['Metabolite', 'Protein', 'Pathway']:
     print('Modifying '+node+' nodes...')
-    IDs[node], OWLs[node] = {}, {}
+    node_data[node] = {}
     node_csvs.extend([path+'neo4jnodes_'+node+'.csv'])
+    desc = {}
+
     with open(path+pathbank_nodes[node]['CSV']) as csvin, open(node_csvs[-1], 'w') as csvout:
         writer = csv.writer(csvout)
         writer.writerow(pathbank_nodes[node]['header'])
-        line_counter = -1
-        for line in csvin:
-            line_counter += 1
-        csvin.seek(0)
         csvin.readline()
-        
-        row_counter = 0
         for row in csv.reader(csvin):
-            row_counter += 1
-            dbId = node+'_'+str(row_counter).zfill(len(str(line_counter))+1)
             ID = row[pathbank_nodes[node]['id_index']]
-            OWL_ID = row[pathbank_nodes[node]['owl_index']]+'-'+ID
-            if ID not in IDs[node]:
-                IDs[node][ID] = []
-            if OWL_ID not in OWLs[node]:
-                OWLs[node][OWL_ID] = []
-            IDs[node][ID].extend([dbId])
-            OWLs[node][OWL_ID].extend([dbId])
+            write_row = row+[node]
 
-            write_row = [dbId]+row+[node]
+            if node != 'Pathway':
+                pathway_species[row[0]] = row[pathbank_nodes[node]['species_index']]
+            else:
+                if ID not in pathway_species:
+                    species = 'Homo sapiens'
+                else:
+                    write_row.insert(pathbank_nodes[node]['species_index'], pathway_species[ID])
+
             write_row = [json.dumps(r.replace('"', '')) for r in write_row]
             write_row = ','.join(write_row)+'\n'
-            csvout.write(write_row)
-
-    IDs[node] = {i: IDs[node][i] for i in IDs[node] if len(IDs[node][i]) > 1}
-    if len(IDs[node]) == 0:
-        continue
-    
-    print('\tRelating replicate nodes ('+str(len(IDs[node]))+')...')
-
-    node_csvs.extend([path+'neo4jnodes_'+node+'_aliases.csv'])
-    edge_csvs.extend([path+'neo4jedges_'+node+'_aliases.csv'])
-    with open(node_csvs[-1], 'w') as nodeout, open(edge_csvs[-1], 'w') as edgeout:
-        nwriter, ewriter = csv.writer(nodeout), csv.writer(edgeout)
-        nwriter.writerow(['dbId:ID', pathbank_nodes[node]['header'][pathbank_nodes[node]['id_index']+1], ':LABEL'])
-        ewriter.writerow([':START_ID', ':END_ID', ':TYPE'])
-
-        for ID in IDs[node]:
-            row_counter += 1
-            dbId = node+'_'+str(row_counter).zfill(len(str(line_counter))+1)
-            nwriter.writerow([dbId, ID, node+'Alias'])
-            for child in IDs[node][ID]:
-                ewriter.writerow([child, dbId, 'nodeAlias'])
-
-with open('OWL_IDs.pkl', 'wb') as pkl:
-    pickle.dump(OWLs, pkl, pickle.HIGHEST_PROTOCOL)
+            if node == 'Pathway':
+                csvout.write(write_row)
+            else:
+                if node == 'Protein':
+                    d = tuple(row[4:11])
+                    if ID in node_data[node]:
+                        m = len([i for i in d if i == ''])
+                        M = len([i for i in desc[ID] if i == ''])
+                        if m < M:
+                            desc[ID] = d
+                            node_data[node][ID] = write_row
+                    else:
+                        desc[ID] = d
+                        node_data[node][ID] = write_row
+                else:
+                    node_data[node][ID] = write_row
+        if node != 'Pathway':
+            for ID in node_data[node]:
+                csvout.write(node_data[node][ID])
 
 
-# OWL_IDs.pkl: {Node: {PathwayID-NodeID: [dbIds]}}
 # add in details like Reactome...
     # already have HMDB, CHEBI IDs
     # link SVG structures, COLMAR IDs, anything else we want, do that here
--- curate_database/convert_pathbankbiopax.py
+++ curate_database/convert_pathbankbiopax.py
@@ -1,112 +1,174 @@
 import xml.etree.ElementTree as ET
 import networkx as nx
+import csv, json, os
 
-filename = 'PW002044.owl'
-
-level = -1
-G = nx.DiGraph() # Add data to Graph object
-skip_elements = ['Ontology', 'imports']
-
-for (event, elem) in ET.iterparse(filename, ['start', 'end']): 
-
-    if event == 'end':
-        level -= 1
-        continue
-    if event == 'start':
-        level += 1
+class PathBankDB_Builder(object):
     
-    if level < 1:
-        continue
+    def __init__(self):
+        self.node_csvs, self.edge_csvs = [], []
 
-    TAG = elem.tag
-    if '{' == TAG[0]:
-        TAG = TAG.split('}')[1]
-    
-    if TAG in skip_elements:
-        continue
-
-    elem.set('pyindex', len(G.nodes))
-    elem.set('tag', TAG)
-    elem.set('level', level)
-    
-    if level == 1:
-        parent = elem.get('pyindex')
-    else:
-        elem.set('XML_parent1', parent)    
-    
-    elem_data = {}
-
-    if elem.text:
-        elem.set('text', elem.text)
-
-    for key, value in elem.items():
-        KEY, VALUE = key, value
-        if '{' == key[0]:
-            KEY = key.split('}')[1]
+    def NodeWriter(self, pathbank_datadict, path):
+        node_data, pathway_species  = {}, {}
         
-        if KEY == 'resource':
-            elem_data['type'] = value[1:].split('/')[0]
-            VALUE = VALUE[1:]
+        for node in ['Metabolite', 'Protein', 'Pathway']:
+            print('Modifying '+node+' nodes...')
+            node_data[node] = {}
+            self.node_csvs.extend([self.path+'neo4jnodes_'+node+'.csv'])
+            desc = {}
 
-        if KEY == 'ID':
-            elem_data['type'] = value.split('/')[0]
-        
-        elem_data[KEY] =  VALUE
+            with open(self.path+pathbank_nodes[node]['CSV']) as csvin, open(self.node_csvs[-1], 'w') as csvout:
+                writer = csv.writer(csvout)
+                writer.writerow(pathbank_nodes[node]['header'])
+                csvin.readline()
+                for row in csv.reader(csvin):
+                    ID = row[pathbank_nodes[node]['id_index']]
+                    write_row = row+[node]
 
-    G.add_nodes_from([(elem.get('pyindex'), elem_data)])
+                    if node != 'Pathway':
+                        pathway_species[row[0]] = row[pathbank_nodes[node]['species_index']]
+                    else:
+                        if ID not in pathway_species:
+                            species = 'Homo sapiens'
+                        else:
+                            write_row.insert(pathbank_nodes[node]['species_index'], pathway_species[ID])
 
-    if parent != elem.get('pyindex'):
-        G.add_edge(parent, elem.get('pyindex'), type='XML_parent1')
+                    write_row = [json.dumps(r.replace('"', '')) for r in write_row]
+                    write_row = ','.join(write_row)+'\n'
+                    if node == 'Pathway':
+                        csvout.write(write_row)
+                    else:
+                        if node == 'Protein':
+                            d = tuple(row[4:11])
+                            if ID in node_data[node]:
+                                m = len([i for i in d if i == ''])
+                                M = len([i for i in desc[ID] if i == ''])
+                                if m < M:
+                                    desc[ID] = d
+                                    node_data[node][ID] = write_row
+                            else:
+                                desc[ID] = d
+                                node_data[node][ID] = write_row
+                        else:
+                            node_data[node][ID] = write_row
+                if node != 'Pathway':
+                    for ID in node_data[node]:
+                        csvout.write(node_data[node][ID])
 
-IDs = {G.nodes[node]['ID']: node for node in G.nodes if 'ID' in G.nodes[node]}
+    def BiopaxLister(self, path):
+        if path[-1] != '/':
+            path = path+'/'
+        filelist = []
+        for PATH, dirs, files in os.walk(path):
+            for filename in files:
+                if filename[-3:] == 'owl':
+                    filelist.extend([path+filename])
+        return filelist
 
-for node in G:
-    if 'resource' in G.nodes[node]:
-        ref = G.nodes[node]['resource']
-        if ref in IDs:
-            G.add_edge(node, IDs[ref], type='referenced')
+    def BiopaxParser(self, filename):
+        level = -1
+        G = nx.DiGraph() # Add data to Graph object
+        skip_elements = ['Ontology', 'imports']
 
-g = nx.DiGraph()
+        for (event, elem) in ET.iterparse(filename, ['start', 'end']): 
 
-pathways = [node for node in G.nodes if G.nodes[node]['tag'] == 'Pathway']
+            if event == 'end':
+                level -= 1
+                continue
+            if event == 'start':
+                level += 1
+            
+            if level < 1:
+                continue
 
-for pathway in pathways:
-    pathwayOrder = [e[1] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'pathwayOrder']
-    pathwayComponent = [e[1] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'pathwayComponent']
-    displayName = [G.nodes[e[1]]['text'] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'displayName']
-    name = [G.nodes[e[1]]['text'] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'name']
-    organism = [G.nodes[e[1]]['resource'] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'organism']
-    xrefs = [G.nodes[e[1]]['resource'] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'xref']
-    for x in range(len(xrefs)):
-        if 'Reference/' in xrefs[x]:
-            xrefs[x] = xrefs[x][len('Reference/'):]
-    print(xrefs)
-# pathwayOrder --> BiochemicalPathwayStep --> stepConversion --> BioChemical Reaction: left, right, name, spontaneous?, stoichiometry, conversionDirection
-# make reaction SVG images using OpenBabel, RDKit?
-exit()
+            TAG = elem.tag
+            if '{' == TAG[0]:
+                TAG = TAG.split('}')[1]
+            
+            if TAG in skip_elements:
+                continue
 
-# Add pathway order to Graph
-for parent in TAGs['Pathway']:
-    displayName, organism = None, None
-    for child in parent:
-        if child.get('tag') == 'displayName':
-            displayName = child.text
-        if child.get('tag') == 'organism':
-            organism = child.get('resource')
-    print(displayName, organism)
-    G.add_nodes_from([(i, elem_data[parent])])
-    pathwayOrder = [c for c in parent if elem_data[int(c)]['tag'] == 'pathwayOrder']
-    for child in pathwayOrder:
-        ID = elem_data[int(child)]['resource'][1:]
+            elem.set('pyindex', len(G.nodes))
+            elem.set('tag', TAG)
+            elem.set('level', level)
+            
+            if level == 1:
+                parent = elem.get('pyindex')
+            else:
+                elem.set('XML_parent1', parent)    
+            
+            elem_data = {}
 
-print(list(tags.keys()))
-        
-# Add SmallMoleculeReference to Graph
-for elem in tags['SmallMoleculeReference']:
-    #print(elem_data[elem])
-    
-    
-    break
-    
-#print(tags.keys())
+            if elem.text:
+                elem.set('text', elem.text)
 
+            for key, value in elem.items():
+                KEY, VALUE = key, value
+                if '{' == key[0]:
+                    KEY = key.split('}')[1]
+                
+                if KEY == 'resource':
+                    elem_data['type'] = value[1:].split('/')[0]
+                    VALUE = VALUE[1:]
+
+                if KEY == 'ID':
+                    elem_data['type'] = value.split('/')[0]
+                
+                elem_data[KEY] =  VALUE
+
+            G.add_nodes_from([(elem.get('pyindex'), elem_data)])
+
+            if parent != elem.get('pyindex'):
+                G.add_edge(parent, elem.get('pyindex'), type='XML_parent1')
+
+        IDs = {G.nodes[node]['ID']: node for node in G.nodes if 'ID' in G.nodes[node]}
+
+        for node in G:
+            if 'resource' in G.nodes[node]:
+                ref = G.nodes[node]['resource']
+                if ref in IDs:
+                    G.add_edge(node, IDs[ref], type='referenced')
+
+        g = nx.DiGraph()
+
+        pathways = [node for node in G.nodes if G.nodes[node]['tag'] == 'Pathway']
+
+        for pathway in pathways:
+            pathwayOrder = [e[1] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'pathwayOrder']
+            pathwayComponent = [e[1] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'pathwayComponent']
+            displayName = [G.nodes[e[1]]['text'] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'displayName']
+            name = [G.nodes[e[1]]['text'] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'name']
+            organism = [G.nodes[e[1]]['resource'] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'organism']
+            xref = [G.nodes[e[1]]['resource'][len('Reference/SMPDB_'):] for e in G.out_edges(pathway) if G.nodes[e[1]]['tag'] == 'xref' and 'Reference/SMPDB_' in G.nodes[e[1]]['resource']]
+            if not xref:
+                continue
+            for node in xref:
+                pass
+
+        # node IDs: Pathway -> SMPDB_ID, Protein -> Uniprot_ID, Metabolite -> PW_ID
+        # pathwayOrder --> BiochemicalPathwayStep --> stepConversion --> BioChemical Reaction: left, right, name, spontaneous?, stoichiometry, conversionDirection
+        # make reaction SVG images using OpenBabel, RDKit?
+
+
+    def Neo4j_Command(self, neo4j_admin_path):
+        neo4j_admin_command = [neo4j_admin_path+'neo4j-admin import ']
+        for file in self.node_csvs:
+            neo4j_admin_command.extend(['--nodes='+file])
+        for file in self.edge_csvs:
+            neo4j_admin_command.extend(['--relationships='+file])
+        print(' '.join(neo4j_admin_command))
+
+
+DBbuilder = PathBankDB_Builder()
+
+# Node writer
+pathbank_nodes = {'Pathway': {'CSV': 'pathbank_pathways.csv', 'id_index': 0, 'species_index': 4, 'header': ['SMPDB_ID:ID', 'PW_ID', 'Pathway_Name', 'Pathway_Subject', 'Species', 'Description', ':LABEL']},
+                'Metabolite': {'CSV': 'pathbank_all_metabolites.csv', 'id_index': 4, 'species_index': 3, 'header': ['SMPDB_ID', 'Pathway_Name', 'Pathway_Subject', 'Species', 'PW_ID:ID', 'Metabolite_Name', 'HMDB_ID', 'KEGG_ID', 'ChEBI_ID', 'DrugBank_ID', 'CAS', 'Formula', 'IUPAC', 'SMILES', 'InChI', 'InChIKey', ':LABEL']},
+                'Protein': {'CSV': 'pathbank_all_proteins.csv', 'id_index': 4, 'species_index': 3, 'header': ['SMPDB_ID', 'Pathway_Name', 'Pathway_Subject', 'Species', 'Uniprot_ID:ID', 'Protein_Name', 'HMDBP_ID', 'DrugBank_ID', 'GenBank_ID', 'Gene_Name', 'Locus', ':LABEL']}}
+#DBbuilder.NodeWriter(pathbank_datadict, path='pathbank_data/')
+
+# List Biopax files
+#biopax_list = DBbuilder.BiopaxLister(path='pathbank_data/pathbank_all_biopax/')
+
+# Parse Biopax files
+for file in ['PW002044.owl']: #biopax_list
+    DBbuilder.BiopaxParser(filename=file)
